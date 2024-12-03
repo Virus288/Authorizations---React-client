58494dd817407f576b92ae4a4231c0a1c829a283
jakub <kiszczyckuba20@wp.pl>
2024-12-03 22:23:34 +0100

    Feat: Added button to remove user's account

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -51,3 +51,51 @@ export const getUserinfo = async (): Promise<string> => {
   console.log('Got error', await res.json());
   return 'None. User is not logged in, or something is wrong';
 };
+
+export const refreshTokens = async (): Promise<void> => {
+  const server = import.meta.env.VITE_API_BACKEND as string;
+  const homeUrl = import.meta.env.VITE_API_HOME as string;
+
+  const res = await fetch(`${server}/user/refresh`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': homeUrl,
+    },
+  });
+
+  if (res.ok) {
+    return;
+  }
+
+  console.log('Got error', await res.json());
+};
+
+export const removeAccount = async (): Promise<void> => {
+  const server = import.meta.env.VITE_API_BACKEND as string;
+  const homeUrl = import.meta.env.VITE_API_HOME as string;
+  const client = import.meta.env.VITE_API_BACKEND_LOGIN_CLIENT as string;
+
+  const query = new URLSearchParams({
+    client,
+  });
+
+  const res = await fetch(`${server}/user?${query.toString()}`, {
+    method: 'DELETE',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': homeUrl,
+    },
+  });
+
+  if (res.ok) {
+    return;
+  }
+
+  console.log('Got error', await res.json());
+
+  const err = (await res.json()) as Error;
+  throw err;
+};
